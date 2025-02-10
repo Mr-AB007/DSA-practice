e01b8a499f514dbec04904f7dd4f4b64daec213d
Anubhav Ranjan <60106478+Mr-AB007@users.noreply.github.com>
2025-02-10 16:51:27 +0000

    minor

--- common-easy-problems/ LongestCommonPrefix.java
+++ common-easy-problems/ LongestCommonPrefix.java
@@ -4,8 +4,7 @@ class LongestCommonPrefix{
     public static String longestCommonPrefix(String[] strs) {
         String prefix = strs[0];
         for(int i = 1 ; i < strs.length ; i++){
-            while(strs[i].indexOf(prefix) != 0){
-                
+            while(strs[i].indexOf(prefix) != 0){ 
                 prefix = prefix.substring(0,prefix.length()-1);
                 if(prefix.length()== 0)
                     return "";
