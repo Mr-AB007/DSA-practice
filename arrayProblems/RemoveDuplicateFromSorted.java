e1aa19330003aeea10944f59160631987cc27955
Anubhav Ranjan <60106478+Mr-AB007@users.noreply.github.com>
2025-02-20 21:01:22 +0530

    Update RemoveDuplicateFromSorted.java

--- arrayProblems/RemoveDuplicateFromSorted.java
+++ arrayProblems/RemoveDuplicateFromSorted.java
@@ -5,6 +5,9 @@ Input: nums = [1,1,2]
 Output: 2, nums = [1,2,_]
 Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
 It does not matter what you leave beyond the returned k (hence they are underscores).
+
+Time:- O(n) array traveasl
+Space:- O(1) no extra space used
 */
 
 class RemoveDuplicateFromSorted {
